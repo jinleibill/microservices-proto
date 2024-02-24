6af356553348d50016e989c4ded5df8d77f24c84
qin <qin@192.168.124.13>
2024-02-24 18:35:12 +0800

    update shell script

--- protoc.sh
+++ protoc.sh
@@ -10,7 +10,6 @@ git config --global user.email "${EMAIL}"
 git fetch --all && git checkout main
 
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
-
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
